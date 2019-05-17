e38cad4f8d55dc5516f511a39ee294cd25bfad00
xiaran <xiaran@polomi.com>
2019-05-17 22:41:00 +0800

    update

--- webhooks.js
+++ webhooks.js
@@ -33,7 +33,7 @@ handler.on('*', function (event) {
     console.log('Received *', event.payload.action);
     //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 })
-
+ 
 handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
